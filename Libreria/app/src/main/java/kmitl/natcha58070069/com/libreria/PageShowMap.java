6084966b7fbdd8c36de0306c4a529a01ce59cac6
Natcha <58070069@kmitl.ac.th>
2017-11-16 01:38:31 +0700

    init project

---
+++ Libreria/app/src/main/java/kmitl/natcha58070069/com/libreria/PageShowMap.java
@@ -0,0 +1,13 @@
+package kmitl.natcha58070069.com.libreria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PageShowMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_show_map);
+    }
+}
